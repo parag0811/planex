d15f4da3450604099699e0ee87bb63af08b974d1
Parag Rangankar <paragrangankar@gmail.com>
2026-03-31 18:00:39 +0530

    updated redis cache worker logic: storing status in redis

--- backend/src/modules/queues/aiWorker.ts
+++ backend/src/modules/queues/aiWorker.ts
@@ -1,5 +1,6 @@
 import { Worker } from "bullmq";
 import { aiHandlers } from "./aiHandler";
+import redis from "../../db/redis";
 
 export const aiWorker = new Worker(
   "ai-queue",
@@ -20,22 +21,68 @@ export const aiWorker = new Worker(
   },
 );
 
-aiWorker.on("active", (job) => {
+aiWorker.on("active", async (job) => {
   console.log(`🚀 Job started: ${job.id} | Type: ${job.name}`);
+
+  const jobId = job.id;
+  const cacheKey = `job:${jobId}`;
+
+  const jobState: JobStatus = {
+    status: "processing",
+  };
+
+  try {
+    await redis.set(cacheKey, JSON.stringify(jobState), "EX", 900);
+  } catch (error) {
+    console.log(`Redis cache failed`);
+  }
 });
 
-aiWorker.on("completed", (job, result) => {
+aiWorker.on("completed", async (job, result) => {
   const duration =
     job.finishedOn && job.processedOn ? job.finishedOn - job.processedOn : null;
 
   console.log(
     ` ✅ Job ${job.id} completed. Type : ${job.name}. Result : ${result}. Duration: ${duration}ms`,
   );
+
+  const jobId = job.id;
+  const cacheKey = `job:${jobId}`;
+
+  const jobState: JobStatus = {
+    status: "completed",
+    result,
+  };
+
+  try {
+    await redis.set(cacheKey, JSON.stringify(jobState), "EX", 900);
+  } catch (error) {
+    console.log(`Redis cache failed`);
+  }
 });
 
-aiWorker.on("failed", (job, error) => {
+aiWorker.on("failed", async (job, error) => {
   console.error(
     ` ❌ Job failed: ${job?.id} | Type: ${job?.name} | Attempt: ${job?.attemptsMade}`,
   );
   console.error(`Error: ${error.message}`);
+
+  if (job == undefined) {
+    console.error("Job is undefined.");
+    return;
+  }
+
+  const jobId = job.id;
+  const cacheKey = `job:${jobId}`;
+
+  const jobState: JobStatus = {
+    status: "failed",
+    error: error.message,
+  };
+
+  try {
+    await redis.set(cacheKey, JSON.stringify(jobState), "EX", 900);
+  } catch (error) {
+    console.log(`Redis cache failed`);
+  }
 });
