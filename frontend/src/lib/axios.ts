9f9f1a9f5c71512f895f2f67e2aee38a0e7cda12
Parag Rangankar <paragrangankar@gmail.com>
2026-04-08 12:35:56 +0530

    init axios setup: interceptors

---
+++ frontend/src/lib/axios.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL as string,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// attach token
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+// res inter -> handle 401
+axiosInstance.interceptors.request.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+
+    return Promise.reject(error);
+  },
+);
+
+export default axiosInstance
